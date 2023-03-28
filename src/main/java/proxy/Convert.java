8005927445374d2bfe0728e75523be0104b4ef6d
EmoNasr <jabarinasredinne@gmail.com>
2023-03-28 22:33:20 +0000

    WebService with JAVA

---
+++ src/main/java/proxy/Convert.java
@@ -0,0 +1,52 @@
+
+package proxy;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for Convert complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>{@code
+ * <complexType name="Convert">
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *         <element name="value" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Convert", propOrder = {
+    "value"
+})
+public class Convert {
+
+    protected double value;
+
+    /**
+     * Gets the value of the value property.
+     * 
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the value property.
+     * 
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+}
